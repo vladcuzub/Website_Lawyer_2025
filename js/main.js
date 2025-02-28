203a89c6eb8d5e7551bd8a81f573911492a56142
vladcuzub <vladalex1989@gmail.com>
2025-02-28 11:35:29 +0100

    cards finished for service section

--- js/main.js
+++ js/main.js
@@ -129,3 +129,30 @@ whatsappButton.addEventListener('click', (event) => {
             whatsappButton.href = `https://wa.me/1234567890`;
       }
 });
+
+
+// Expand Card
+
+document.addEventListener("DOMContentLoaded", function () {
+      let cards = document.querySelectorAll(".box"); // Selectează toate cardurile
+
+      cards.forEach(function (card) {
+            let button = card.querySelector(".read_more_btn");
+            let hiddenText = card.querySelector(".hidden_text");
+
+            if (!button || !hiddenText) return; // Dacă nu există elementele, ieșim
+
+            button.addEventListener("click", function () {
+                  card.classList.toggle("expanded");
+
+                  if (card.classList.contains("expanded")) {
+                        hiddenText.style.display = "block"; // Afișează textul
+                        button.textContent = "Citeste mai putin";
+                  } else {
+                        hiddenText.style.display = "none"; // Ascunde textul
+                        button.textContent = "Citeste mai mult";
+                  }
+            });
+      });
+});
+
